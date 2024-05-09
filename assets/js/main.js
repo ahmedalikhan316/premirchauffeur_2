f0b4835691d15591590460a58b5fde84b80d4405
ahmedalikhan316 <ahmedalikhan.316@gmail.com>
2024-05-09 16:04:05 +0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1012,3 +1012,28 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 });
+
+
+//carousel services javascript
+$('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 4;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
